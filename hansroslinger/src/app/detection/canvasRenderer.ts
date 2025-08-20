64618e1cda19aaf4ce016d43b21cca77e1015153
StephSS14 <stephanie.sisilia@gmail.com>
2025-08-20 04:43:02 +0000

    feat(gesture): Draw only thumb and index finger tip on canvas

--- hansroslinger/src/app/detection/canvasRenderer.ts
+++ hansroslinger/src/app/detection/canvasRenderer.ts
@@ -1,5 +1,4 @@
 import {
-  HandLandmarker,
   DrawingUtils,
   GestureRecognizerResult,
 } from "@mediapipe/tasks-vision";
@@ -21,11 +20,10 @@ export const canvasRenderer = async (
   canvasCtx!.clearRect(0, 0, canvas.width, canvas.height);
 
   handLandmarks.landmarks.forEach((landmarks) => {
-    drawingUtils.drawConnectors(landmarks, HandLandmarker.HAND_CONNECTIONS, {
-      color: "#00FF00",
-      lineWidth: 5,
-    });
-    drawingUtils.drawLandmarks(landmarks, {
+    // Only draw thumb and index finger tip
+    // Index 4: thumb tip
+    // Index 8: index finger tip
+    drawingUtils.drawLandmarks([landmarks[4], landmarks[8]], {
       color: "#FF0000",
       lineWidth: 2,
     });
